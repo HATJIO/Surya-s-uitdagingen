3da4d3883a2fbe695fb76033947c2dcb012766b3
HATJIO <95481768+HATJIO@users.noreply.github.com>
2022-10-28 00:48:21 +0200

    Add project files.

---
+++ Surya's uitdagingen/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Surya_s_uitdagingen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+
+    class Con1
+    {
+        //1. Pythagoras method
+        
+
+        //2. driehoek in console
+  
+
+        //3a. kopieer
+       
+
+        //3b. druiventros van nullen in console
+    
+    }
+
+}
